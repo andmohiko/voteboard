5dcbcd68b9fd44eb7517f2167f630e5f86843968
andmohiko <andmohiko@gmail.com>
2025-02-17 19:48:50 +0900

    fix: frontend build

--- apps/frontend/src/components/Layouts/SpLayout.tsx
+++ apps/frontend/src/components/Layouts/SpLayout.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, ReactNode } from 'react'
+import type { ReactElement, ReactNode } from 'react'
 
 import { FlexBox } from '~/components/Base/FlexBox'
 import { PageHead } from '~/components/Head'
